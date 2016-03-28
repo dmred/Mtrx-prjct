e5e841da020ba62393979bc1669450dc2306c1c1
dmred <dmredchenko@gmail.com>
2016-03-28 17:16:21 +0300

    Update init.cpp

--- Matrix/init.cpp
+++ Matrix/init.cpp
@@ -7,8 +7,8 @@ SCENARIO("Matrix init", "[init]") {
 		auto rows = 3;
 		auto columns = 3;
 		WHEN("Create instansce of Matrix") {
-			CMatrix<int> m(rows, columns);
-			CMatrix<int> a;
+			matrix<int> m(rows, columns);
+			matrix<int> a;
 			THEN("The number of rows and columns must be preserved") {
 				REQUIRE(m.rowsNumber() == rows);
 				REQUIRE(m.columnsNumber() == columns);
@@ -20,76 +20,59 @@ SCENARIO("Matrix init", "[init]") {
 }
 
 SCENARIO("Matrix: operator +", "[addition]") {
-	CMatrix<int> A, B, expected;
+	matrix<int> A, B, expected;
 	A.readFromFile("A2x2.txt");
 	B.readFromFile("B2x2.txt");
 	expected.readFromFile("A+B.txt");
 
-	CMatrix<int> result = A + B;
+	matrix<int> result = A + B;
 	REQUIRE(result == expected);
 }
 
-SCENARIO("Matrix: operator -", "[subtraction]") {
-	CMatrix<int> A, B, expected;
-	A.readFromFile("A2x2.txt");
-	B.readFromFile("B2x2.txt");
-	expected.readFromFile("A-B.txt");
-
-	CMatrix<int> result = A - B;
-	REQUIRE(result == expected);
-}
 
 SCENARIO("Matrix: operator *(matix, number)", "[multiplication]") {
-	CMatrix<int> A, expected;
+	matrix<int> A, expected;
 	A.readFromFile("A2x2.txt");
 	expected.readFromFile("A2x2mult2.txt");
 
-	CMatrix<int> result = A * 2;
+	matrix<int> result = A * 2;
 	REQUIRE(result == expected);
 }
 
 SCENARIO("Matrix: operator *(matix, matrix)", "[multiplication]") {
-	CMatrix<int> A, B, expected;
+	matrix<int> A, B, expected;
 	A.readFromFile("A2x2.txt");
 	B.readFromFile("B2x2.txt");
 	expected.readFromFile("AxB.txt");
 
-	CMatrix<int> result = A * B;
+	matrix<int> result = A * B;
 	REQUIRE(result == expected);
 }
 
-SCENARIO("Matrix: operator [](index)", "[get row]") {
-	CMatrix<int> A;
+SCENARIO("Matrix: operator [](i)", "[get row]") {
+	matrix<int> A;
 	int* a = new int[2]; a[0] = 1; a[1] = 2;
 	A.readFromFile("A2x2.txt");
 
 	bool flag = true;
 	int* res = A[0];
-	for (int i = 0; i < 2; i++) {
-		if (a[i] != res[i]) {
+	for (int j = 0; j < 2; j++) {
+		if (a[j] != res[j]) {
 			flag = false;
 		}
 	}
 	REQUIRE(flag);
 }
 
-SCENARIO("Matrix: operator ==", "[equal]") {
-	CMatrix<int> A, B;
-	A.readFromFile("A2x2.txt");
-	B.readFromFile("A2x2.txt");
-	bool flag = (A == B);
 
-	REQUIRE(flag);
+SCENARIO("Matrix: get_num_rows()", "[rows number]") {
+	matrix<int> A(3, 3);
+
+	REQUIRE(A.get_num_rows() == 3);
 }
 
-SCENARIO("Matrix: rowsNumber()", "[rows number]") {
-	CMatrix<int> A(3, 3);
+SCENARIO("Matrix: get_num_cols()", "[columns number]") {
+	matrix<int> A(3, 3);
 
-	REQUIRE(A.rowsNumber() == 3);
-}
-
-SCENARIO("Matrix: columnsNumber()", "[columns number]") {
-	CMatrix<int> A(3, 3);
-
-	REQUIRE(A.columnsNumber() == 3);
+	REQUIRE(A.get_num_cols() == 3);
 }
